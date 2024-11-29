f3d5ac96706e28d161152f152372243318cd78ce
Carlos Peña <gesaodin@gmail.com>
2024-11-29 16:41:07 -0400

    Comprobantes: Inversiones y Comisionees

--- src/app/pages/principal/operaciones/procesooperaciones/procesooperaciones.component.ts
+++ src/app/pages/principal/operaciones/procesooperaciones/procesooperaciones.component.ts
@@ -3,6 +3,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ApiService, IAPICore } from 'src/app/services/apicore/api.service';
+import { FID_IComprobante } from 'src/app/services/banfanb/comprobante.service';
 import { UtilService } from 'src/app/services/util/util.service';
 
 @Component({
@@ -30,6 +31,20 @@ export class ProcesooperacionesComponent implements OnInit {
   }
 
   events: string[] = [];
+  public Comprobante: FID_IComprobante = {
+    plan: 0,
+    codigo: "",
+    descripcion: "",
+    detalle: "",
+    fecha_operacion: "",
+    fecha_ejercicio: "",
+    debe: 0,
+    haber: 0,
+    llave: ''
+  };
+
+  visible : boolean = false
+
   constructor(private apiService: ApiService,
     private _snackBar: MatSnackBar,
     private ngxService: NgxUiLoaderService,
@@ -37,23 +52,40 @@ export class ProcesooperacionesComponent implements OnInit {
     public formatter: NgbDateParserFormatter) { }
 
   ngOnInit(): void {
-    let d = new Date().toISOString().substring(0,10).split('-')
-    this.fechaultimo = d[2] + '/' + d[1] + '/' + d[0]
     this.consultarUltimoCierre()
   }
 
 
-  consultarUltimoCierre(){
+  
+
+  consultarUltimoCierre() {
     this.ngxService.stopLoader('load-precierre')
     this.xAPI.funcion = "FID_CUltimoCierre"
     this.xAPI.parametros = ''
     this.xAPI.valores = ''
-
+    // console.log('hola')
     this.apiService.Ejecutar(this.xAPI).subscribe(
       async data => {
-          let ultc = data.Cuerpo
-          if (ultc.length > 0 ) this.fechaultimo = this.util.ConvertirFechaHumana( ultc[0].ultimo_cierre)
-          this.ngxService.stopLoader('load-precierre')
+
+        let ultc = data.Cuerpo
+        if (ultc.length > 0) {
+          let fecha = ultc[0].fecha_cierre;
+          let d = fecha.split('-');
+          this.fechaultimo = d[2] + '/' + d[1] + '/' + d[0];
+          let fechaCierre = new Date(`${d[0]}-${d[1]}-${d[2]}`);
+          fechaCierre.setDate(fechaCierre.getDate() + 2);
+          fechaCierre.setHours(0, 0, 0, 0);
+          this.fechai = fechaCierre;   
+          this.fechaf = fechaCierre;
+          this.dias = 1
+        }
+
+        //console.log(this.fechaultimo)
+        // if ( this.fechaultimo == "30/06/2024" || this.fechaultimo == "31/12/2024"  ) {
+        //   this.semestral = true
+        // }
+        this.ngxService.stopLoader('load-precierre')
+
       },
       (error) => {
         console.log(error)
@@ -61,26 +93,26 @@ export class ProcesooperacionesComponent implements OnInit {
     )
   }
 
-
   consultarComisiones(){
 
     let dia = parseInt( this.dias.toString() )
     this.ngxService.stopLoader('load-precierre')
     this.xAPI.funcion = "FID_CalcularComision"
     this.xAPI.parametros = dia + ',360'
-
-    console.log(this.xAPI)
     this.xAPI.valores = ''
-    console.log('Control de datas')
-
+    this.visible = false
+    this.acum_debe = 0
+    this.acum_haber = 0
+    
     this.apiService.Ejecutar(this.xAPI).subscribe(
       async data => {
-          console.log(data)
           this.lstComisiones = data.Cuerpo
-         
-          
+          this.lstComisiones.map( e => {
+            this.acum_debe += parseFloat(e.calculo_capital)
+            this.acum_haber += parseFloat(e.calculo_capital)
+          })
+          if(this.lstComisiones.length > 0 ) this.visible = true
           this.ngxService.stopLoader('load-precierre')
-        
       },
       (error) => {
         console.log(error)
@@ -93,28 +125,23 @@ export class ProcesooperacionesComponent implements OnInit {
   }
 
 
-  CalcularDevengo(){
-    if (this.fechai == undefined || this.fechaf == undefined) {
-      this._snackBar.open('Recuerde seleccionar un rango de fechas', 'OK')
-      return
-    }
-    let fini = this.util.ConvertirFechaDB(this.fechai)
-    let ffin = this.util.ConvertirFechaDB(this.fechaf)
-    let fope = new Date().toISOString().substring(0,10)
-    let usuario = ''
-    let llave = ''
+  // CalcularDevengo(){
+  //   if (this.fechai == undefined || this.fechaf == undefined) {
+  //     this._snackBar.open('Recuerde seleccionar un rango de fechas', 'OK')
+  //     return
+  //   }
+  //   let fini = this.util.ConvertirFechaDB(this.fechai)
+  //   let ffin = this.util.ConvertirFechaDB(this.fechaf)
+  //   let fope = new Date().toISOString().substring(0,10)
+  //   let usuario = ''
+  //   let llave = ''
 
-    this.ngxService.startLoader('load-cont')
-    this.xAPI.funcion = "FID_BashIntereses"
-    this.xAPI.parametros = fope + ',' + fini + ',' + ffin + ',' + usuario + ',' + llave
-    this.xAPI.valores = ''
-  }
+  //   this.ngxService.startLoader('load-cont')
+  //   this.xAPI.funcion = "FID_BashIntereses"
+  //   this.xAPI.parametros = fope + ',' + fini + ',' + ffin + ',' + usuario + ',' + llave
+  //   this.xAPI.valores = ''
+  // }
   
-   ListarDevengo(){
-
-
-   }
-
 
    getMoneda(numero: number): string {
     return this.util.ConvertirMoneda(numero);
@@ -123,6 +150,66 @@ export class ProcesooperacionesComponent implements OnInit {
   getCodigo(id ) : string{
     return "CON-" + this.util.zfill(id, 4)
   }
+
+
+
+
+  
+  GenerarComprobante(){
+
+    
+    let fecha = this.util.ConvertirFechaDB(this.fechai)
+
+    this.Comprobante = {
+      plan: 1,
+      codigo: this.util.GenerarUnicId(),
+      descripcion: `COMISIONES ADMINISTRATIVAS ${this.util.ConvertirFechaHumana(fecha)}`,
+      detalle: `COMISIONES ADMINISTRATIVAS ${this.util.ConvertirFechaHumana(fecha)}`,
+      fecha_operacion: this.util.ConvertirFechaDB(this.fechai),
+      fecha_ejercicio: this.util.ConvertirFechaDB(this.fechai),
+      debe: this.acum_debe,
+      haber: this.acum_haber,
+      llave: 'M'
+    }
+
+    this.xAPI.funcion = "FID_IComprobante"
+    this.xAPI.parametros = ''
+    this.xAPI.valores = JSON.stringify(this.Comprobante)
+    
+    console.log(this.Comprobante)
+    // this.InsertData(cant)
+
+    this.apiService.Ejecutar(this.xAPI).subscribe(
+      data => {
+        console.log(data)
+        this.InsertData(data, this.lstComisiones.length) 
+      },
+      (error) => {
+        console.log(error)
+        this.ngxService.stopLoader('load-cont')
+      }
+    )
+
+  }
+
+  InsertData(dt, cant : number){
+    let fecha = this.util.ConvertirFechaDB(this.fechai)
+    cant++
+    this.xAPI.funcion = "FID_IComisionesAdministrativas"
+    this.xAPI.parametros = dt.msj  + ',360,1,' + fecha
+    this.xAPI.valores = ''
+    this.apiService.Ejecutar(this.xAPI).subscribe(
+      data => {
+        console.log(data)
+        // this.InsertData(dt, cant)
+      },
+      (error) => {
+        console.log(error)
+        this.ngxService.stopLoader('load-cont')
+      }
+    )
+  }
+
 
 
 }
--- src/app/pages/principal/operaciones/procesos/procesos.component.ts
+++ src/app/pages/principal/operaciones/procesos/procesos.component.ts
@@ -6,6 +6,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { NgbDate, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ApiService, IAPICore } from 'src/app/services/apicore/api.service';
+import { FID_IComprobante } from 'src/app/services/banfanb/comprobante.service';
 import { LPosicionInversiones } from 'src/app/services/banfanb/contabilidad.service';
 import { UtilService } from 'src/app/services/util/util.service';
 
@@ -16,22 +17,12 @@ import { UtilService } from 'src/app/services/util/util.service';
 })
 export class ProcesosComponent implements OnInit {
 
-  public ELEMENT_DATA: LPosicionInversiones[] = [];
-  displayedColumns: string[] = [
-    "codigo",
-    "instrumento",
-    "valor_nominal",
-    "costo_adquisicion",
-    "interes_diario",
-    "interes_acumulado",
-  ];
-  dataSource: any;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  
+ 
   public fechau: any
   public fechaultimo = ''
   public fechai: any
   public fechaf: any
+  public fecha_al : string = ''
 
   public lstAsientos = []
   public bcuentat = false
@@ -44,7 +35,23 @@ export class ProcesosComponent implements OnInit {
     parametros: '',
   }
 
-  events: string[] = [];
+  events: string[] = []
+
+  public Comprobante: FID_IComprobante = {
+    plan: 0,
+    codigo: "",
+    descripcion: "",
+    detalle: "",
+    fecha_operacion: "",
+    fecha_ejercicio: "",
+    debe: 0,
+    haber: 0,
+    llave: ''
+  };
+
+  visible : boolean = false
+
+  // public lstData = []
   
   constructor( private apiService: ApiService,
     private _snackBar: MatSnackBar,
@@ -53,26 +60,41 @@ export class ProcesosComponent implements OnInit {
     public formatter: NgbDateParserFormatter,) { }
 
   ngOnInit(): void {
-    let d = new Date().toISOString().substring(0,10).split('-')
-    this.fechaultimo = d[2] + '/' + d[1] + '/' + d[0]
+    // let d = new Date().toISOString().substring(0,10).split('-')
+    // this.fechaultimo = d[2] + '/' + d[1] + '/' + d[0]
     this.consultarUltimoCierre()
     
   }
 
-  consultarUltimoCierre(){
+
+  consultarUltimoCierre() {
     this.ngxService.stopLoader('load-precierre')
     this.xAPI.funcion = "FID_CUltimoCierre"
     this.xAPI.parametros = ''
     this.xAPI.valores = ''
-
+    // console.log('hola')
     this.apiService.Ejecutar(this.xAPI).subscribe(
       async data => {
 
-          let ultc = data.Cuerpo
-          if (ultc.length > 0 ) this.fechaultimo = this.util.ConvertirFechaHumana( ultc[0].ultimo_cierre)
-          
-          this.ngxService.stopLoader('load-precierre')
-        
+        let ultc = data.Cuerpo
+        if (ultc.length > 0) {
+          let fecha = ultc[0].fecha_cierre;
+          let d = fecha.split('-');
+          this.fechaultimo = d[2] + '/' + d[1] + '/' + d[0];
+          let fechaCierre = new Date(`${d[0]}-${d[1]}-${d[2]}`);
+          fechaCierre.setDate(fechaCierre.getDate() + 2);
+          fechaCierre.setHours(0, 0, 0, 0);
+          this.fechai = fechaCierre;   
+          this.fechaf = fechaCierre;
+          this.dias = 1
+        }
+
+        //console.log(this.fechaultimo)
+        // if ( this.fechaultimo == "30/06/2024" || this.fechaultimo == "31/12/2024"  ) {
+        //   this.semestral = true
+        // }
+        this.ngxService.stopLoader('load-precierre')
+
       },
       (error) => {
         console.log(error)
@@ -80,10 +102,16 @@ export class ProcesosComponent implements OnInit {
     )
   }
 
+
   CalcularDias(type: string, event: MatDatepickerInputEvent<Date>){
     this.dias = this.util.CalcuarDiasTranscurridos(this.fechai, this.fechaf)+1
   }
 
+  CalcularDiasInterese(fechai, fechaf): number {
+    let calculo = this.util.CalcuarDiasTranscurridos(fechai, fechaf) + 1
+
+    return calculo
+  }
 
 
   CalculoPosicion(){
@@ -154,7 +182,8 @@ export class ProcesosComponent implements OnInit {
     }
     let fini = this.util.ConvertirFechaDB(this.fechai)
     let ffin = this.util.ConvertirFechaDB(this.fechaf)
-    this.ELEMENT_DATA = []
+   
+    this.fecha_al = fini
     this.ngxService.startLoader('load-cont')
     this.xAPI.funcion = "FID_CPosicionInversiones"
     this.xAPI.parametros = fini + ',' + ffin
@@ -162,18 +191,24 @@ export class ProcesosComponent implements OnInit {
 
     this.apiService.Ejecutar(this.xAPI).subscribe(
       async data => {
-        
-        this.ELEMENT_DATA = data.Cuerpo
-        this.dataSource = new MatTableDataSource<LPosicionInversiones>(
-          this.ELEMENT_DATA
-        );
-        this.dataSource.paginator = this.paginator;
-        this.ConsultarAsientos()
+        console.log(data)
+        this.lstAsientos = data.Cuerpo
+        data.Cuerpo.forEach(e => {
+          this.acum_debe += parseFloat( e.interes_acumulado) 
+          this.acum_haber += parseFloat( e.interes_acumulado) 
+        });
+
+       
+        if (this.lstAsientos.length > 0) {
+         this.visible = true
+        }
+       
+        // this.ConsultarAsientos()
         // this.fechai = ''
         // this.fechaf = ''
         // await this.ngxService.stopLoader('load-cont')
         // this.apiService.Mensaje('Proceso exitoso', 'Felicitaciones', 'success', 'inversion')
-        
+        await this.ngxService.stopLoader('load-cont')
         
       },
       (error) => {
@@ -186,36 +221,87 @@ export class ProcesosComponent implements OnInit {
 
 
 
-  ConsultarAsientos(){
+  // ConsultarAsientos(){
 
-    if (this.fechai == undefined || this.fechaf == undefined) {
-      this._snackBar.open('Recuerde seleccionar un rango de fechas', 'OK')
-      return
+  //   if (this.fechai == undefined || this.fechaf == undefined) {
+  //     this._snackBar.open('Recuerde seleccionar un rango de fechas', 'OK')
+  //     return
+  //   }
+  //   let fini = this.util.ConvertirFechaDB(this.fechai)
+  //   let ffin = this.util.ConvertirFechaDB(this.fechaf)
+  
+  //   this.ngxService.startLoader('load-cont')
+  //   this.xAPI.funcion = "FID_CTotalizadorCuentas"
+  //   this.xAPI.parametros = fini + ',' + ffin
+  //   this.xAPI.valores = ''
+  //   this.acum_debe = 0
+  //   this.acum_haber = 0
+  //   this.apiService.Ejecutar(this.xAPI).subscribe(
+  //     async data => {
+  //       console.log(data)
+        
+  //       await this.ngxService.stopLoader('load-cont')
+  //       // this.apiService.Mensaje('Proceso exitoso', 'Felicitaciones', 'success', 'inversion')
+        
+        
+  //     },
+  //     (error) => {
+  //       console.log(error)
+  //       this.ngxService.stopLoader('load-cont')
+  //     }
+  //   )
+  // }
+
+  getCuenta(e){
+    return e.codigo_padre + '.' + e.parte + '.' + e.moneda + '.' + e.nivel_1 + '.' + e.nivel_2 + '.' + e.nivel_3 + '.' + e.nivel_4 + '.' + e.nivel_5
+  }
+
+  GenerarComprobante(){
+
+    
+    let fecha = this.util.ConvertirFechaDB(this.fechai)
+
+    this.Comprobante = {
+      plan: 1,
+      codigo: this.util.GenerarUnicId(),
+      descripcion: `DEVENGO DE INVERSIONES ${this.util.ConvertirFechaHumana(fecha)}`,
+      detalle: `DEVENGO DE INVERSIONES ${this.util.ConvertirFechaHumana(fecha)}`,
+      fecha_operacion: this.util.ConvertirFechaDB(this.fechai),
+      fecha_ejercicio: this.util.ConvertirFechaDB(this.fechai),
+      debe: this.acum_debe,
+      haber: this.acum_haber,
+      llave: 'M'
     }
-    let fini = this.util.ConvertirFechaDB(this.fechai)
-    let ffin = this.util.ConvertirFechaDB(this.fechaf)
-    this.ELEMENT_DATA = []
-    this.ngxService.startLoader('load-cont')
-    this.xAPI.funcion = "FID_CTotalizadorCuentas"
-    this.xAPI.parametros = fini + ',' + ffin
-    this.xAPI.valores = ''
-    this.acum_debe = 0
-    this.acum_haber = 0
-    this.apiService.Ejecutar(this.xAPI).subscribe(
-      async data => {
-        
-        this.lstAsientos = data.Cuerpo
-        data.Cuerpo.forEach(e => {
-          this.acum_debe += parseFloat( e.debe) 
-          this.acum_haber += parseFloat( e.haber) 
-        });
 
-        this.fechai = ''
-        this.fechaf = ''
-        await this.ngxService.stopLoader('load-cont')
-        // this.apiService.Mensaje('Proceso exitoso', 'Felicitaciones', 'success', 'inversion')
-        
-        
+    this.xAPI.funcion = "FID_IComprobante"
+    this.xAPI.parametros = ''
+    this.xAPI.valores = JSON.stringify(this.Comprobante)
+    
+    console.log(this.Comprobante)
+    // this.InsertData(cant)
+
+    this.apiService.Ejecutar(this.xAPI).subscribe(
+      data => {
+        console.log(data)
+        this.InsertData(data, this.lstAsientos.length) 
+      },
+      (error) => {
+        console.log(error)
+        this.ngxService.stopLoader('load-cont')
+      }
+    )
+
+  }
+
+  InsertData(dt, cant : number){
+    cant++
+    this.xAPI.funcion = "FID_IDevengoInversiones"
+    this.xAPI.parametros = dt.msj + ',' + 1
+    this.xAPI.valores = ''
+    this.apiService.Ejecutar(this.xAPI).subscribe(
+      data => {
+        console.log(data)
+        // this.InsertData(dt, cant)
       },
       (error) => {
         console.log(error)
@@ -224,9 +310,24 @@ export class ProcesosComponent implements OnInit {
     )
   }
 
-  getCuenta(e){
-    return e.codigo_padre + '.' + e.parte + '.' + e.moneda + '.' + e.nivel_1 + '.' + e.nivel_2 + '.' + e.nivel_3 + '.' + e.nivel_4 + '.' + e.nivel_5
+  getMoneda(numero: number): string {
+    let valor = this.util.ConvertirMoneda(numero)
+    let result = valor.toString()
+    let cant = result.split(',')
+   
+    if (cant.length == 1 )
+      result = result + ',00'
+
+    return result
   }
 
+  InteresDiarioCupon(inv: any, dias: number): number {
+    
+    let rendicion =
+      ((inv.valor_nominal * inv.tasa_cupon * (1 / 100)) / inv.base_calculo) * dias
+
+    
+    return parseFloat(rendicion.toFixed(2));
+  }
 
 }
