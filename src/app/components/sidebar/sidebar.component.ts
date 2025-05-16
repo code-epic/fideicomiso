9c4509b02e32281408513cecf3f0f8d63f3fc41d
Adrian <adrian.dryzara3@gmail.com>
2025-05-16 15:51:30 -0400

    Fecha de ultimo cierre y precierre en sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService, IAPICore } from 'src/app/services/apicore/api.service';
 import { LoginService } from 'src/app/services/seguridad/login.service';
 import { MensajeService } from 'src/app/services/util/mensaje.service';
 
@@ -20,17 +21,17 @@ export class SidebarComponent implements OnInit {
 
   public menuItems: any[];
   public isCollapsed = true;
+  public ultimoCierre: string = ''
+  public ultimoPreCierre: string = ''
 
-  constructor(private router: Router, 
+  constructor(
+    private router: Router, 
     private mesageService : MensajeService,
-    private loginService: LoginService) { 
-
-  }
+    private loginService: LoginService,
+    private apiService: ApiService
+  ) { }
 
   async ngOnInit() {
-
-    
-    
     if (ROUTES.length == 0){
       await this.loginService.Iniciar()
       var App = this.loginService.Aplicacion
@@ -60,6 +61,58 @@ export class SidebarComponent implements OnInit {
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
     });
+
+    this.consultarPreCierre()
+    this.consultarCierre()
+  }
+
+  consultarPreCierre() {
+    let xAPI: IAPICore = {
+      funcion: '',
+      parametros: '',
+      valores: ''
+    }
+
+    xAPI.funcion = 'FID_CFechaMaxPreCierre'
+    xAPI.parametros = ''
+    xAPI.valores = ''
+
+    this.apiService.Ejecutar(xAPI).subscribe(
+      data => {
+        if (data.Cuerpo != undefined ){
+          this.ultimoPreCierre = data.Cuerpo[0].fecha          
+        }
+      },
+      err => {
+        console.error(err);
+      }
+    )
+  }
+
+  consultarCierre() {
+    let xAPI: IAPICore = {
+      funcion: '',
+      parametros: '',
+      valores: ''
+    }
+    xAPI.funcion = "FID_CUltimoCierre"
+    xAPI.parametros = ''
+    xAPI.valores = ''
+
+    this.apiService.Ejecutar(xAPI).subscribe(
+      async data => {
+
+        let ultc = data.Cuerpo
+        if (ultc.length > 0) {
+          let fecha = ultc[0].fecha_cierre;
+          let d = fecha.split('-');
+          this.ultimoCierre = d[0] + '-' + d[1] + '-' + d[2];          
+        }
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
   }
 
   msj(pagina : string){
